c6825194f3ea7a81e5e6838f4df9aa1821c6515a
Megan E Bishop <161883992+melizbishop@users.noreply.github.com>
2025-06-17 11:43:33 -0400

    Create entropy_calc.c

---
+++ src/entropy_calc.c
@@ -0,0 +1,14 @@
+
+double entropy_calc(double* alpha, int len) {
+    double alpha0 = 0.0;
+    double sum_gammaln = 0.0;
+    double sum_term = 0.0;
+
+    for (int i = 0; i < len; i++) {
+        alpha0 += alpha[i];
+        sum_gammaln += lgamma(alpha[i]);
+        sum_term += (alpha[i] - 1.0) * digamma(alpha[i]);
+    }
+
+    return sum_gammaln - lgamma(alpha0) + (alpha0 - len) * digamma(alpha0) - sum_term;
+}
