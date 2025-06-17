0b477b74a961a41ee08a0f800218d7a541882fe3
Megan E Bishop <161883992+melizbishop@users.noreply.github.com>
2025-06-17 12:44:36 -0400

    Update entropy_calc.c

--- src/entropy_calc.c
+++ src/entropy_calc.c
@@ -1,15 +1,39 @@
 #include "MCMCglmm.h"
+#include <math.h>
+double digamma(double x) {
+    double result = 0.0, xx, xx2, xx4;
+    for (; x < 7.0; ++x) result -= 1.0/x;
+    x -= 1.0/2.0;
+    xx = 1.0/x;
+    xx2 = xx*xx;
+    xx4 = xx2*xx2;
+    result += log(x) + (1.0/24.0)*xx2 - (7.0/960.0)*xx4 + (31.0/8064.0)*xx4*xx2 - (127.0/30720.0)*xx4*xx4;
+    return result;
+}
 
-double entropy_calc(double* alpha, int len) {
-    double alpha0 = 0.0;
-    double sum_gammaln = 0.0;
-    double sum_term = 0.0;
-
+double dirichlet_entropy(double *alpha, int len) {
+    double alpha0 = 0.0, entropy = 0.0;
     for (int i = 0; i < len; i++) {
         alpha0 += alpha[i];
-        sum_gammaln += lgamma(alpha[i]);
-        sum_term += (alpha[i] - 1.0) * digamma(alpha[i]);
     }
 
-    return sum_gammaln - lgamma(alpha0) + (alpha0 - len) * digamma(alpha0) - sum_term;
+    double log_beta = 0.0;
+    double digamma_alpha0 = digamma(alpha0);
+
+    for (int i = 0; i < len; i++) {
+        log_beta += lgamma(alpha[i]);
+    }
+    log_beta -= lgamma(alpha0);
+
+    entropy = log_beta - (alpha0 - len) * digamma_alpha0;
+    for (int i = 0; i < len; i++) {
+        entropy += (alpha[i] - 1.0) * digamma(alpha[i]);
+    }
+
+    return entropy;
+}
+
+double entropy_penalty(double target_entropy, double current_entropy, double intensity) {
+    double diff = (current_entropy - target_entropy) / target_entropy;
+    return intensity * diff * diff;
 }
