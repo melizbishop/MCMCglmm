419f190013982b6e3cbea009812c5b0fd5b22c67
Megan E Bishop <161883992+melizbishop@users.noreply.github.com>
2025-06-17 13:25:11 -0400

    Update entropy_calc.c

--- src/entropy_calc.c
+++ src/entropy_calc.c
@@ -1,17 +1,26 @@
 #include "MCMCglmm.h"
 #include <math.h>
+#include <stdlib.h>  // for malloc and free
+
 double digamma(double x) {
     double result = 0.0, xx, xx2, xx4;
-    for (; x < 7.0; ++x) result -= 1.0/x;
-    x -= 1.0/2.0;
-    xx = 1.0/x;
-    xx2 = xx*xx;
-    xx4 = xx2*xx2;
-    result += log(x) + (1.0/24.0)*xx2 - (7.0/960.0)*xx4 + (31.0/8064.0)*xx4*xx2 - (127.0/30720.0)*xx4*xx4;
+    for (; x < 7.0; ++x) result -= 1.0 / x;
+    x -= 1.0 / 2.0;
+    xx = 1.0 / x;
+    xx2 = xx * xx;
+    xx4 = xx2 * xx2;
+    result += log(x) + (1.0 / 24.0) * xx2 - (7.0 / 960.0) * xx4 +
+              (31.0 / 8064.0) * xx4 * xx2 - (127.0 / 30720.0) * xx4 * xx4;
     return result;
 }
 
 double dirichlet_entropy(double *logalpha, int len) {
+    double *alpha = (double *)malloc(len * sizeof(double));
+    if (alpha == NULL) {
+        Rprintf("Memory allocation failed in dirichlet_entropy\n");
+        return 0.0;
+    }
+
     double alpha0 = 0.0, entropy = 0.0;
     for (int i = 0; i < len; i++) {
         alpha[i] = exp(logalpha[i]);
@@ -30,12 +39,11 @@ double dirichlet_entropy(double *logalpha, int len) {
     for (int i = 0; i < len; i++) {
         entropy += (alpha[i] - 1.0) * digamma(alpha[i]);
     }
-    
+
     free(alpha);
     return entropy;
 }
 
 double entropy_penalty(double target, double current, double intensity) {
     return intensity * pow((current - target) / target, 2.0);
-}
 }
